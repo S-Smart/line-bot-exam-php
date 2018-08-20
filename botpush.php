d1c3d4baf1bf9d1253d7810137bf28d4c301d4ad
S-Smart <42536961+S-Smart@users.noreply.github.com>
2018-08-20 22:40:45 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'PTKs1qARADFbxymTqYk2B2mADMAPJ3682bYxHc6bK0qJUXAWxMONLprw7WTtoAMvcMI8Pg1slbQyJNZspE40izNjimVqC0s2bmgkoLILLIy023GkqvogB3wWmEbPScWSmSsS5skK16IDMLcGsAxZzgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'a4257578a963eaddcb26ce220438c3f6';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U647c7f453213a7093de3778c295edb80';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
