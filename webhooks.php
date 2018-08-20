f14637f58d87f4747cb4399638703c7643ce8efd
S-Smart <42536961+S-Smart@users.noreply.github.com>
2018-08-20 16:44:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,8 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'BtJ/unr8lSAuJx7T2hg3Pm31YQpTER161ZzhGWMSta4s+ZG4tyHHqMvv8BJsigQ6cMI8Pg1slbQyJNZspE40izNjimVqC0s2bmgkoLILLIwOdiJBQSFekRVpMjvS1CD6sBtikEWBk1Dc1ENRZD3G7gdB04t89/1O/w1cDnyilFU='
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
